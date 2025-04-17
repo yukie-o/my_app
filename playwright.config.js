8e97ad9099de7ee4d9e3fafc1054d3962717c2b2
otsukay <otsukay@soum.co.jp>
2025-04-17 16:11:03 +0900

    first commit

---
+++ playwright.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  webServer: {
+    command: 'npm start',
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    baseURL: 'http://localhost:3000',
+  },
+});
